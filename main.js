1b085f463e1d9b7fdefe713c9ec53286aeb01847
Flex RP <flexdesigns2@gamail.com>
2020-09-22 22:13:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -85,7 +85,11 @@ client.on('message', message =>{
       });
 
 
-
+      client.on('message', message => {
+        if (message.content === 'what is my avatar') {
+          message.reply(message.author.displayAvatarURL());
+        }
+      });
 
 
 
